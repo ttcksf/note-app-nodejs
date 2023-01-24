18161dea455eb3d1f658222d266887489f3f58ad
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2023-01-25 07:56:12 +0900

    done

--- app.js
+++ app.js
@@ -2,13 +2,36 @@ require("dotenv").config();
 
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
-const { reset } = require("nodemon");
+const methodOverride = require("method-override");
+const connectDB = require("./server/config/db");
+const session = require("express-session");
+const passport = require("passport");
+const MongoStore = require("connect-mongo");
 
 const app = express();
 const port = 3001 || process.env.PORT;
 
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+    }),
+    // cookie: { maxAge: new Date(Date.now() + 3600000) },
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride("_method"));
+
+// connect DB
+connectDB();
 
 // static files
 app.use(express.static("public"));
@@ -19,6 +42,7 @@ app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
 // Routes
+app.use("/", require("./server/routes/auth"));
 app.use("/", require("./server/routes/index"));
 app.use("/", require("./server/routes/dashboard"));
 
